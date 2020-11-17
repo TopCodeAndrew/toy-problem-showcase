0ce0cae653acc0b8a02a5e26fbb2f4cb9ab08783
Andrew Smith <topcodeandrew@gmail.com>
2020-11-17 16:51:11 -0700

    second commit

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,35 @@
 import React, {Component} from 'react';
 
 export default class FilterString extends Component {
+    constructor(){
+        super();
+        this.state = {
+            unFilteredArray: '',
+            userInput: '',
+            filteredArray: [],
+        }
+    }
+
+    handleChange(input){
+        this.state.userInput = input;
+    }
+
+    filterString(){
+
+    }
+
+
+
+
     render(){
         return(
-            <p> FilterString Component </p>
+            <div>
+                <h4 className = "puzzleBox filterStringPB"> Filter String </h4>
+                <span className></span>
+                <input className = "inputLine" onChange = {(e) => this.handleChange(e.target.value)} />
+                <button className = "confirmationButton" onClick = {() => this.filterString(this.state.userInput) } />
+                <span className = "resultsBox filterStringRB"></span>
+            </div>
         )
     }
 }
